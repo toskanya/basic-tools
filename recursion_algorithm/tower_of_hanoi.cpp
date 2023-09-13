7a77d25d2ea58fcff25a1116faf4a3e5fc0bb18b
toskanya <trandanghienlong2004@gmail.com>
2023-09-13 23:00:21 +0700

    add algorithms

---
+++ recursion_algorithm/tower_of_hanoi.cpp
@@ -0,0 +1,24 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+
+
+int towerOfHanoi(int total_disks, string from, string to, string aux) {
+
+    if (total_disks == 1) {
+        cout << "move " << from << " to " << to << endl;;
+        return 1;
+    }
+    else {
+        return towerOfHanoi(total_disks - 1, from, aux, to) + towerOfHanoi(1, from, to, aux) + towerOfHanoi(total_disks - 1, aux, to, from);
+    }
+}
+
+int main() {
+    int steps = towerOfHanoi(3, "A", "C", "B");
+
+    cout << endl << steps;
+
+    return 0;
+}
